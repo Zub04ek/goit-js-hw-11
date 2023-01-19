bae11dae99d4a57fc9aa2e57d07f462896e3239d
Zub04ek <110388961+Zub04ek@users.noreply.github.com>
2023-01-19 11:30:20 +0000

    Deploying to gh-pages from @ Zub04ek/goit-js-hw-11@fc497e61a543bec20207c7a853f530d63e85e7f2 🚀

--- index.64743282.js
+++
@@ -1,2 +0,0 @@
-const t=document.querySelector("body"),e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]");e.addEventListener("click",(function(){o=setInterval((()=>{const e=`#${Math.floor(16777215*Math.random()).toString(16)}`;t.style.backgroundColor=e}),1e3),e.disabled=!0,n.disabled=!1})),n.addEventListener("click",(function(){clearInterval(o),e.disabled=!1,n.disabled=!0}));let o=null;
-//# sourceMappingURL=index.64743282.js.map
