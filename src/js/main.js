fc497e61a543bec20207c7a853f530d63e85e7f2
Zub04ek <nastasyz@ukr.net>
2023-01-19 13:29:40 +0200

    add libraries

---
+++ src/js/main.js
@@ -0,0 +1,8 @@
+import axios from 'axios';
+import Notiflix from 'notiflix';
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
+
+console.log(axios);
+console.log(Notiflix);
+console.log(SimpleLightbox);
