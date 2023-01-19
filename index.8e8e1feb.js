74321fe139225e0f392e827ee2a3d402573a119c
Zub04ek <110388961+Zub04ek@users.noreply.github.com>
2023-01-19 11:01:22 +0000

    Deploying to gh-pages from @ Zub04ek/goit-js-hw-11@0ae0a7b699cee2931d9a75be5252112a451f37ad 🚀

---
+++ index.8e8e1feb.js
@@ -0,0 +1,2 @@
+!function(){var t=document.querySelector("body"),e=document.querySelector("button[data-start]"),n=document.querySelector("button[data-stop]");e.addEventListener("click",(function(){a=setInterval((function(){var e="#".concat(Math.floor(16777215*Math.random()).toString(16));t.style.backgroundColor=e}),1e3),e.disabled=!0,n.disabled=!1})),n.addEventListener("click",(function(){clearInterval(a),e.disabled=!1,n.disabled=!0}));var a=null}();
+//# sourceMappingURL=index.8e8e1feb.js.map
